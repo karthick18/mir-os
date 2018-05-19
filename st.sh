a8a6c6777d14ad7894f9227c78abbd555d20749c
A R Karthick <kramanar@ciena.com>
2018-05-18 18:39:16 -0700

    Use loopback 2 to avoid usage conflicts

--- st.sh
+++ st.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 img=${1:-grubimg}
 mkdir -p /mir_img
-losetup -d /dev/loop0 1>/dev/null 2>&1
-losetup /dev/loop0 $img
-mount /dev/loop0 /mir_img 2>/dev/null
+losetup -d /dev/loop2 1>/dev/null 2>&1
+losetup /dev/loop2 $img
+mount /dev/loop2 /mir_img 2>/dev/null
 cp mir.elf /mir_img/boot
 cp -r modules /mir_img
 umount /mir_img
-losetup -d /dev/loop0 1>/dev/null 2>&1
+losetup -d /dev/loop2 1>/dev/null 2>&1
 
